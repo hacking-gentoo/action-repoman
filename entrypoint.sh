74f6642bfcf4cf49add44956751351343930e5af
Max Hacking <max@hacking.co.uk>
2019-11-04 02:27:46 +0000

    Added copyright notice

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,28 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -z "${GITHUB_WORKSPACE}" ]]; then
-    echo "Must set GITHUB_WORKSPACE in env"
+function die()
+{
+    echo "::error::$1"
+	echo "------------------------------------------------------------------------------------------------------------------------"
     exit 1
-fi
+}
 
-cd "${GITHUB_WORKSPACE}" || exit 2
+cat << END
+------------------------------------------------------------------------------------------------------------------------
+                                _   _                                                              
+                      __ _  ___| |_(_) ___  _ __        _ __ ___ _ __   ___  _ __ ___   __ _ _ __  
+                     / _\` |/ __| __| |/ _ \| '_ \ _____| '__/ _ \ '_ \ / _ \| '_ \` _ \ / _\` | '_ \\ 
+                    | (_| | (__| |_| | (_) | | | |_____| | |  __/ |_) | (_) | | | | | | (_| | | | |
+                     \__,_|\___|\__|_|\___/|_| |_|     |_|  \___| .__/ \___/|_| |_| |_|\__,_|_| |_|
+                                                                |_|                                
+                    https://github.com/hacking-gentoo/action-repoman           (c) 2019 Max Hacking 
+------------------------------------------------------------------------------------------------------------------------
+END
 
-PORTDIR_OVERLAY="." repoman --straight-to-stable -dx full
+[[ -z "${GITHUB_WORKSPACE}" ]] && die "Must set GITHUB_WORKSPACE in env"
+cd "${GITHUB_WORKSPACE}" || die "GITHUB_WORKSPACE does not exist"
+
+PORTDIR_OVERLAY="." repoman --straight-to-stable -dx full || die "Repoman failed"
+
+echo "------------------------------------------------------------------------------------------------------------------------"
